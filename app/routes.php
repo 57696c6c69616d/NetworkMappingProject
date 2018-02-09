040624a6de41b8ef71a833528f5cf229afc5c992
57696c6c69616d <33284899+57696c6c69616d@users.noreply.github.com>
2018-02-09 12:34:24 +0100

    Graph is now refreshing

--- app/routes.php
+++ app/routes.php
@@ -81,3 +81,49 @@ $app->match('/graph', function(Request $request) use ($app) {
 
     return $app['twig']->render('graph.html.twig', array('localIP' => $localIP, 'nbIP' => $nbIP, 'nbPackets' => $nbPackets, 'nbTCPPackets' => $nbTCPPackets, 'nbUDPPackets' => $nbUDPPackets, 'percentage' => $percentage, 'first_date' => $first_date, 'last_date' => $last_date, 'totalLength' => $totalLength, 'machineForm' => $machineForm->createView()));
 })->bind('graph');
+
+// Data page
+$app->get('/data', function () use ($app) {
+	#Graph
+	$file = fopen('cap\cap.json', 'w+');
+	$json = '{"nodes": ';
+	if (fwrite($file, $json) === false) { 
+		echo "Cannot write to text file. <br />";
+	}
+	$test = $json;
+
+	$data = $app['dao.machine']->findAll();
+	$json = json_encode($data);
+	if (fwrite($file, $json) === false) { 
+		echo "Cannot write to text file. <br />"; 
+	}
+	$test = $test . $json;
+	if (fwrite($file, ', "links": ') === false) { 
+		echo "Cannot write to text file. <br />"; 
+	}
+	$test = $test . ', "links": ';
+	
+	$data = $app['dao.packet']->findAll();
+	$json = json_encode($data, JSON_NUMERIC_CHECK);
+	if (fwrite($file, $json) === false) { 
+		echo "Cannot write to text file. <br />"; 
+	}
+	$test = $test . $json;
+	if (fwrite($file, ', "info": ') === false) { 
+		echo "Cannot write to text file. <br />"; 
+	}
+	$test = $test . ', "info": ';
+
+	$data = $app['dao.packet']->getInfo();
+	$json = json_encode($data, JSON_NUMERIC_CHECK);
+	if (fwrite($file, $json) === false) { 
+		echo "Cannot write to text file. <br />"; 
+	}
+	$test = $test . $json;
+	if (fwrite($file, '}') === false) { 
+		echo "Cannot write to text file. <br />"; 
+	}
+	$test = $test . '}';
+
+	return $test;
+})->bind('data');
